f31afc7b02ce1a11bb4d96adc28f62fdd958a0b7
Jerex17 <jeremias2007ps@gmail.com>
2024-11-06 19:50:36 -0400

    actualizacion

--- Portafolio/My Code Snippets/SQLQuery2.2sql.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE DATABASE Serie_del_caribe_2023
-
-GO
-
-USE Serie_del_caribe_2023
-
-CREATE TABLE Conjuntos_14_02_23(Numero numeric,Paises varchar (40),JuegoGanado Integer,JuegoPerido Integer,Porcentaje_De_Bateo Decimal)
